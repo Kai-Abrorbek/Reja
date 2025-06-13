dc63282a94d07cd4eedc5ddc436ea8683e8ddebc
Kai-Abrorbek <abror0dev@gmail.com>
2025-06-13 22:29:16 +0900

    fix: Rejalarni o'zgartirish (UPDATE) & MITASK-D

--- train.js
+++ train.js
@@ -150,6 +150,7 @@
 // const result = countStr("123asdasdW12");
 // console.log(result);
 
+// ========================================================== //
 
 /* 
   MITASK-C 
@@ -160,90 +161,117 @@
     tushgan vaqt ham log qilinsin.
 */
 
-// masalani yechimi:
+// // masalani yechimi:
 
-const moment = require("moment");
-const nowTime = moment().format("hh:mm");
-class Shop{
-  #product_1;
-  #product_1_Cnt;
-  #product_2;
-  #product_2_Cnt;
-  #product_3;
-  #product_3_Cnt;
-  constructor(product_1, product_2, product_3){
-    this.#product_1 = product_1,
-    this.#product_1_Cnt = 0,
-    this.#product_2 = product_2,
-    this.#product_2_Cnt = 0,
-    this.#product_3 = product_3,
-    this.#product_3_Cnt = 0
-  }
+// const moment = require("moment");
+// const nowTime = moment().format("hh:mm");
+// class Shop{
+//   #product_1;
+//   #product_1_Cnt;
+//   #product_2;
+//   #product_2_Cnt;
+//   #product_3;
+//   #product_3_Cnt;
+//   constructor(product_1, product_2, product_3){
+//     this.#product_1 = product_1,
+//     this.#product_1_Cnt = 0,
+//     this.#product_2 = product_2,
+//     this.#product_2_Cnt = 0,
+//     this.#product_3 = product_3,
+//     this.#product_3_Cnt = 0
+//   }
 
-  qabul(product, proCount){
-    if(typeof product === "string" && typeof proCount === "number"){
-      switch (product) {
-        case this.#product_1:
-          this.#product_1_Cnt += proCount;
-          break;
-        case this.#product_2:
-          this.#product_2_Cnt += proCount;
-          break
-        case this.#product_3:
-          this.#product_3_Cnt += proCount;
-          break
-        default:
-          console.log("Notogri maxsulot kiritdingiz!");
-      }
-    }else{
-      console.log("Notogri malumot kiritdingiz");
-    }
-  };
+//   qabul(product, proCount){
+//     if(typeof product === "string" && typeof proCount === "number"){
+//       switch (product) {
+//         case this.#product_1:
+//           this.#product_1_Cnt += proCount;
+//           break;
+//         case this.#product_2:
+//           this.#product_2_Cnt += proCount;
+//           break
+//         case this.#product_3:
+//           this.#product_3_Cnt += proCount;
+//           break
+//         default:
+//           console.log("Notogri maxsulot kiritdingiz!");
+//       }
+//     }else{
+//       console.log("Notogri malumot kiritdingiz");
+//     }
+//   };
 
-  sotish(product, proCount){
-    if(typeof product === "string" && typeof proCount === "number"){
-      switch (product) {
-        case this.#product_1:
-          this.#product_1_Cnt -= proCount;
-          break;
-        case this.#product_2:
-          this.#product_2_Cnt -= proCount;
-          break
-        case this.#product_3:
-          this.#product_3_Cnt -= proCount;
-          break
-        default:
-          console.log("Notogri maxsulot kiritdingiz!");
-      }
-    }else{
-      console.log("Notogri malumot kiritdingiz");
-    }
-  };
+//   sotish(product, proCount){
+//     if(typeof product === "string" && typeof proCount === "number"){
+//       switch (product) {
+//         case this.#product_1:
+//           this.#product_1_Cnt -= proCount;
+//           break;
+//         case this.#product_2:
+//           this.#product_2_Cnt -= proCount;
+//           break
+//         case this.#product_3:
+//           this.#product_3_Cnt -= proCount;
+//           break
+//         default:
+//           console.log("Notogri maxsulot kiritdingiz!");
+//       }
+//     }else{
+//       console.log("Notogri malumot kiritdingiz");
+//     }
+//   };
 
-  qoldiq(){
-    return `Horir ${nowTime}da: 
-    ${this.#product_1_Cnt}ta ${this.#product_1},
-    ${this.#product_2_Cnt}ta ${this.#product_2} va 
-    ${this.#product_3_Cnt}ta ${this.#product_3} mavjud!`
-  };
+//   qoldiq(){
+//     return `Horir ${nowTime}da: 
+//     ${this.#product_1_Cnt}ta ${this.#product_1},
+//     ${this.#product_2_Cnt}ta ${this.#product_2} va 
+//     ${this.#product_3_Cnt}ta ${this.#product_3} mavjud!`
+//   };
+// }
+
+// const newShop = new Shop("shashlik", "non", "somsa");
+
+// newShop.qabul("shashlik", 5);
+// newShop.qabul("non", 15);
+// newShop.qabul("somsa", 25);
+// newShop.sotish("somsa", 5);
+
+// const qoldiq = newShop.qoldiq();
+// console.log(qoldiq);
+
+
+// const newShop2 = new Shop("qazi", "shorva", "osh");
+// newShop2.qabul("qazi", 5);
+// newShop2.qabul("shorva", 15);
+// newShop2.qabul("osh", 25);
+// newShop2.sotish("shorva", 5);
+
+// const qoldiq2 = newShop2.qoldiq();
+// console.log(qoldiq2);
+
+// ========================================================== //
+
+/*
+  Shunday function tuzing, u 2ta string parametr ega bolsin,
+  hamda agar har ikkala string bir hil harflardan iborat bolsa 
+  true aks holda false qaytarsin.
+*/
+
+// masala yechimi:
+function checkContent(str1, str2){
+  if (str1.length !== str2.length) return false;
+  /* Pastdagi codeni chozib yozilishi!!
+    let list1 = str1.split("");
+    let list2 = str2.split("");
+    let list1sort = list1.sort();
+    let list2sort = list2.sort();
+    let list1join = list1sort.join('')
+    let list2join = list2sort.join('')
+
+    return list1join === list2join;
+  */
+  
+  return str1.split('').sort().join('') === str2.split('').sort().join('');
 }
-
-const newShop = new Shop("shashlik", "non", "somsa");
-
-newShop.qabul("shashlik", 5);
-newShop.qabul("non", 15);
-newShop.qabul("somsa", 25);
-newShop.sotish("somsa", 5);
-
-const qoldiq = newShop.qoldiq();
-console.log(qoldiq);
-
-
-const newShop2 = new Shop("qazi", "shorva", "osh");
-newShop2.qabul("qazi", 5);
-newShop2.qabul("shorva", 15);
-newShop2.qabul("osh", 25);
-newShop2.sotish("shorva", 5);
-
-const qoldiq2 = newShop2.qoldiq();
-console.log(qoldiq2);
+const result = checkContent("sssdaa", "aadsss");
+console.log(result);
