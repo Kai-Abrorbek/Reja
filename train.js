ee9a33be7244f7f2b8ca4eb1e1d36c3183f25b9a
Kai <abror0dev@gmail.com>
2025-06-15 18:51:05 +0900

    Update train.js

--- train.js
+++ train.js
@@ -1,3 +1,19 @@
+/*
+  E-TASK: 
+  Shunday function tuzing, u bitta string argumentni qabul qilib 
+  osha stringni teskari qilib return qilsin.
+  MASALAN: getReverse("hello") return qilsin "olleh"
+*/
+
+function getReverse(str){
+  return str.split('').reverse().join("");
+}
+
+const strReverse = getReverse("hello world");
+console.log(strReverse);
+
+// ≈============================== //
+
 // console.log("Jack Ma maslahatlari");
 // const list = [
 //   "yaxshi talaba bo'ling", // 0-20
@@ -277,17 +293,3 @@
 // console.log(result);
 
 // ========================================================== //
-
-/*
-  E-TASK: 
-  Shunday function tuzing, u bitta string argumentni qabul qilib 
-  osha stringni teskari qilib return qilsin.
-  MASALAN: getReverse("hello") return qilsin "olleh"
-*/
-
-function getReverse(str){
-  return str.split('').reverse().join("");
-}
-
-const strReverse = getReverse("hello world");
-console.log(strReverse);
